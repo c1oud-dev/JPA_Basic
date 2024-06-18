73144d4db4feb1131bff502f060be3230727333e
c1oud-dev <gksmf0187@gmail.com>
2024-06-18 18:16:18 +0900

    [JPA Basic] registration, update, delete, find

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,19 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
-        emf.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); //트랜잭션 시작
+        try {
+            Member findmember = em.find(Member.class, 1L);
+            findmember.setName("HelloJPA");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close(); //엔티티 매니저 종료
+        }
+        emf.close(); //엔티티 매니저 팩토리 종료
     }
 }
---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity //이걸 꼭 넣어줘야 한다. 그래야 jpa가 처음 로딩될 때 jpa를 사용하는 얘라고 인식
+public class Member {
+    @Id //어노테이션 작업을 할 때 꼭 persistence로 넣어주기
+    private Long id; //id랑 name을 넣는 이유는 현재 db에 있기 때문
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
